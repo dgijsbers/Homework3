90a05977651c391526e002ca78094327574115ee
Demery Gijsbers <demgijs@umich.edu>
2017-10-08 19:34:16 -0400

    done I think

--- new_application/new.py
+++ new_application/new.py
@@ -9,7 +9,19 @@ def form1():
 	return render_template('color_form.html')
 
 @app.route('/colorinfo',methods = ['POST', 'GET'])
-def results():
+def p_results():
 	if request.method == 'GET':
 		results = request.args
-		return render_template('results.html', results = results)
+	primary = {"red", "blue", "yellow"}
+	for x in primary:
+		if x == results['color']:
+			return render_template('primary.html', results = results)
+	secondary = {"green", "orange", "purple"}
+	for y in secondary:
+		if y ==results['color']:
+			return render_template('secondary.html', results = results)
+		if y not in results['color']:
+			return render_template('unique.html', results = results)
+
+
+
